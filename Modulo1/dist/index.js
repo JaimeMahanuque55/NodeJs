5a789f4255db7af271973d127e166647012ea403
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-05-23 20:17:53 +0200

    Configuring the Typescript

---
+++ Modulo1/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = "Jaime";
+let idade = 105;
+console.log(`Meu nome e ${nome} e tenho ${idade} anos`);
--- Modulo1/src/index.ts
+++ Modulo1/src/index.ts
@@ -1,4 +1,4 @@
 let nome: string = "Jaime";
-let idade: number = 25;
+let idade: number = 15;
 
 console.log(`Meu nome e ${nome} e tenho ${idade} anos`);
