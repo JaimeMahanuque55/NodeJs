072b4e7eb6c7cc3e64e7b4ee000f7a1d6373782a
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-05-24 21:58:35 +0200

    Working with Routes

--- Modulo1/src/server.ts
+++ Modulo1/src/server.ts
@@ -1,13 +1,47 @@
+// import express, { Request, Response } from 'express';
+
+// const server = express();
+
+// server.get('/', (req: Request, res: Response) => {
+//     res.send("Hello World!")
+// });
+
+// server.get('/contato', (req: Request, res: Response) => {
+//     res.send("Hello World, contacto!")
+// });
+
+// server.listen(3000);
+
+
+/////////////////// Working with routes //////////////////////////
+
 import express, { Request, Response } from 'express';
 
 const server = express();
 
+// GET, POST, PUT, DELETE
+
+// const HomePage = (req: Request, res: Response) => {
+//     res.send('Hello World')
+// }
+
+
 server.get('/', (req: Request, res: Response) => {
-    res.send("Hello World!")
+    res.send('Hello World!');
 });
 
-server.get('/contato', (req: Request, res: Response) => {
-    res.send("Hello World, contacto!")
+server.get('/noticia/:slug', (req: Request, res: Response) => {
+
+    let slug: string = req.params.slug;
+    res.send(`Noticia: ${slug}`);
 });
 
-server.listen(3000);
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response) => {
+
+    let { origem, destino } = req.params;
+
+    res.send(`Procurando Voos de ${origem.toUpperCase()} ate ${destino.toUpperCase()}`);
+});
+
+server.listen(80);
