59bd92cf774fc16a07841017ed72848330d1619e
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-05-30 21:11:07 +0200

    Mustache: Else of Lists

--- Modulo1/src/routes/index.ts
+++ Modulo1/src/routes/index.ts
@@ -99,7 +99,7 @@ export default router;
 */
 
 ///////////////////// Mustache: Displaying Lists (loop) //////////////////////////
-
+/*
 import { Router, Request, Response } from 'express';
 
 const router = Router();
@@ -125,6 +125,45 @@ router.get('/', (req: Request, res: Response) => {
         dayCotes: [
             "Work hard",
             "Don't give up for nothing"
+        ]
+    });
+});
+
+router.get('/contacto', (req: Request, res: Response) => {
+    res.send("Formulario de contacto");
+});
+
+router.get('/sobre', (req: Request, res: Response) => {
+    res.send("Pagina institucional sobre a empresa");
+});
+
+export default router;
+*/
+///////////////////// Mustache: Else of Lists //////////////////////////
+
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+
+    let age: number = 15;
+    let showOld: boolean = false;
+
+    if (age >= 30) {
+        showOld = true
+    }
+
+    res.render('home', {
+        user: 'James',
+        age: 30,
+        showOld,
+        products: [
+            { title: 'Gelinhos', price: 20 },
+            { title: 'Maguinha', price: 30 },
+            { title: 'Frozy', price: 15 }
+        ],
+        dayCotes: [
         ]
     });
 });
